d209c423b1eb60386e67ea41949abda2c9cf0104
Rasyidi - ROG <rasyidialwee@gmail.com>
2022-03-22 10:06:21 +0800

    init

---
+++ resources/views/todo/index.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>My List</h1>
+
+                <div class="card">
+                    <div class="card-body">
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Item</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Makan</td>
+                                    <td>Not Done</td>
+                                    <td><button class="btn btn-primary">Done</button></td>
+                                </tr>
+                                <tr>
+                                    <td>Makan</td>
+                                    <td>Not Done</td>
+                                    <td><button class="btn btn-primary">Done</button></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+                <div class="card mt-5">
+                    <div class="row text-white">
+                        <div class="col bg-primary">Ini Col 4</div>
+                        <div class="col bg-success">Ini Col 4</div>
+                        <div class="col bg-warning">Ini Col 4</div>
+                        <div class="col bg-danger">Ini Col 4</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\TodoController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/trainer', function () {
+//     return 2 + 2 / 2;
+// });
+
+
+// Route::get('trainer', [TestController::class, 'cubaan']);
+
+Route::get('todos', [TodoController::class, 'index']);
+
+// Dari route ke view terus
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Dari route ke controller
+// Route::get('/', [UserController::class, 'index']);
+
+// Resource Route
+// Route::resource('user', UserController::class);
