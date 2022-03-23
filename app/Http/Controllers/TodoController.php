68ed45203ea96122e4e3771e6d5e0830ff69af6a
whomyme <whomyme@gmail.com>
2022-03-23 10:36:32 +0800

    edit & update

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -71,6 +71,9 @@ class TodoController extends Controller
     public function edit(Todo $todo)
     {
         //
+        return view('todo.edit', [
+            'todo' => $todo
+        ]);
     }
 
     /**
@@ -83,6 +86,12 @@ class TodoController extends Controller
     public function update(Request $request, Todo $todo)
     {
         //
+        // dd($request->all(), $todo);
+
+        $todo->name = $request->name;
+        $todo->save();
+
+        return redirect('/todos')->with('status', 'Todo ' . $request->name . ' Updated');
     }
 
     /**
--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -45,12 +45,17 @@
                                         <td>{{ $todos->firstItem() + $key }}</td>
                                         <td>{{ $todo->name }}</td>
                                         <td>{{ $todo->status }}</td>
-                                        <td><button class="btn btn-primary">Done</button>
-                                            <form action="todos/{{ $todo->id }}" method="post">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-danger">Delete</button>
-                                            </form>
+                                        <td>
+                                            <div class="d-flex">
+                                                <a href="/todos/{{ $todo->id }}/edit"
+                                                    class="btn btn-warning mx-2">Edit</a>
+                                                <button class="btn btn-primary mx-2">Done</button>
+                                                <form action="todos/{{ $todo->id }}" method="post">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                                </form>
+                                            </div>
                                         </td>
                                     </tr>
                                 @empty
